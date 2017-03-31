d105e63c31bd920297470d29cf2e7db3c2318c84
Dashon-Hawkins <dashon.hawkins@gmail.com>
2017-03-31 10:41:05 -0700

    2nd Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require ('express');
+var bodyParser = require ('body-parser');
+var music = require ('./music.json');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/music', function(req, res){
+  res.send(music);
+});
+
+app.get('/music/:id', function(req, res) {
+  var musicIndex = req.params.id;
+  res.send(music[musicIndex]);
+});
+
+app.post('/music', function(req, res) {
+  music.push(req.body);
+  res.send(music);
+});
+
+app.listen(8000, function() {
+  console.log("Heard it on 8000");
+});
